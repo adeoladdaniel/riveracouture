b462fd7668a1570a6334713c4e96fb5a26c6d565
Adeola rotimi <adeola@Adeolas-MacBook-Pro.local>
2023-01-17 12:31:56 +0100

    riveracouture

---
+++ build/precache-manifest.ef440d3d2d5b0ac53a896a43d3970dd1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24cfc184fbbadd84c9775f731ff1de32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dfd37060b3a1d3612c92",
+    "url": "/static/css/main.260181bb.chunk.css"
+  },
+  {
+    "revision": "75bfa24d9513e3987292",
+    "url": "/static/js/2.a1180dd0.chunk.js"
+  },
+  {
+    "revision": "bc4eb88f8227368b1203ec44207e353c",
+    "url": "/static/js/2.a1180dd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e47c8b4b6d827586576e",
+    "url": "/static/js/3.69494319.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/3.69494319.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e33ac46243487fd847c",
+    "url": "/static/js/4.54680fe0.chunk.js"
+  },
+  {
+    "revision": "dfd37060b3a1d3612c92",
+    "url": "/static/js/main.06cae266.chunk.js"
+  },
+  {
+    "revision": "daf992cc61d447fe8f3a",
+    "url": "/static/js/runtime-main.8961ec9b.js"
+  }
+]);
